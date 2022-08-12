cc98c90e61a3454968c99ac67bfef72cfbb7120f
Heet Sankesara <heetsankesara3@gmail.com>
2022-08-12 15:23:36 +0100

    updated mock code

--- mock-features/features.py
+++ mock-features/features.py
@@ -1,13 +1,30 @@
-from radarpipeline.features.feature import FeatureGroup, Feature
+# insert at 1, 0 is the script path (or '' in REPL)
+import sys
+sys.path.insert(1, '../radar-pipeline/')
+from radarpipeline.features import FeatureGroup, Feature
+
 from radarpipeline.datalib import RadarData
 import pandas  as pd
 
 class MockFeatureGroup(FeatureGroup):
-    def __init__(self, name: str, description: str):
+    def __init__(self, name="x", description="x"):
         self.name = "MockFeatureGroup"
         self.description = "contains mock features"
         self.features = [PhoneBatteryChargingDuration, StepCountPerDay]
+        super().__init__(self.name, self.description, self.features)
         self.required_input_data = self._compute_required_data()
+
+    def preprocess(self, data: RadarData) -> RadarData:
+        """
+        Preprocess the data for each feature in the group.
+        """
+        pass
+
+    def compute_features(self, data: RadarData) -> RadarData:
+        """
+        compute and combine the features for each feature in the group.
+        """
+        pass
 
 class PhoneBatteryChargingDuration(Feature):
     def __init__(self):
